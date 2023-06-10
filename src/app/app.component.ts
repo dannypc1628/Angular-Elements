b8e07f08fd3aacad2c0f1e1789d7360abc4fa2fb
dannyLiu <h152074716@gmail.com>
2023-06-10 18:03:14 +0800

    避免 XSS，所以將 HTML 字串消毒

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { AlertComponent } from './alert/alert.component';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,16 @@ import { AlertComponent } from './alert/alert.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  content = '';
+  content!: SafeHtml;
 
-  constructor(injector: Injector) {
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {
     const AlertElement = createCustomElement(AlertComponent, { injector });
     // customElements 是原生 JS 的 API
     customElements.define('app-alert', AlertElement);
     setTimeout(() => {
-      this.content = "<app-alert message='這是一個警報範例'></app-alert>";
+      this.content = domSanitizer.bypassSecurityTrustHtml(
+        "<app-alert message='這是一個警報範例'></app-alert>"
+      );
     }, 1000);
   }
 }
