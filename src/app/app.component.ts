939cc296c5e7fab7b2437edeec432b0dace1bcaf
dannyLiu <h152074716@gmail.com>
2023-06-10 17:18:22 +0800

    使用 innerHTML timeout後插入標準HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular-Elements';
+  content = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.content = '<p>這是一個警報訊息</p>';
+    }, 1000);
+  }
 }
