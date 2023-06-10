3c4e01710bcf36ecb84fd7f509fb2afbe5e88025
dannyLiu <h152074716@gmail.com>
2023-06-10 17:41:13 +0800

    使用 Angular Elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   content = '';
 
-  constructor() {
+  constructor(injector: Injector) {
+    const AlertElement = createCustomElement(AlertComponent, { injector });
+    // customElements 是原生 JS 的 API
+    customElements.define('app-alert', AlertElement);
     setTimeout(() => {
       this.content = "<app-alert message='這是一個警報範例'></app-alert>";
     }, 1000);
